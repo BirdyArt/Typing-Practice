132a46f3d1e431f986a0efab961151b469d9baf3
BirdyArt <asobolev91@gmail.com>
2021-05-29 11:18:35 -0700

    Added restart and generate

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -5,7 +5,15 @@ const Paragraph = () => {
   const [clickText, setClickText] = useState("Click to start");
   const [currentKey, setCurrentKey] = useState(0);
   const [text, setText] = useState(null);
+  const [flag, setFlag] = useState(true);
 
+  useEffect(() => {
+    fetch("http://metaphorpsum.com/paragraphs/1/3")
+    .then(resp => resp.text())
+    .then(setCurrentText)
+    console.log(resp => resp)
+  }, [flag])
+  
   const handleKeyDown = (e) => {
     if (e.key === text[currentKey].props.children) {
       setText(currentText.split('').map((el,i) => i <= currentKey ? <span style={{backgroundColor: "green"}} key={i}>{el}</span> : <span style={{backgroundColor: "white"}} key={i}>{el}</span> ));
@@ -13,21 +21,25 @@ const Paragraph = () => {
     }
   }
 
-  const handleOnclick = () => {
-    setClickText("");
+  const handleRestart = () => {
+    console.log(currentText)
+    setClickText("Click to restart");
+    setText(currentText.split('').map((el,i) => <span key={i}>{el}</span>));
+    setCurrentKey(0);
+        console.log('hi')
+
+  };
+
+  const handleGenerate = () => {
+    setFlag(!flag);
     setText(currentText.split('').map((el,i) => <span key={i}>{el}</span>));
   }
 
-  useEffect(() => {
-    fetch("http://metaphorpsum.com/paragraphs/2/4")
-    .then(resp => resp.text())
-    .then(setCurrentText)
-  }, [])
-
   return (
-    <div className="paragraph" tabIndex="0" onClick={handleOnclick} onKeyDown={handleKeyDown}>
+    <div className="paragraph" tabIndex="0" onKeyDown={handleKeyDown}>
       <p className="text">{text}</p>
-      <h2>{clickText}</h2>
+      <h2 onClick={handleRestart}>{clickText}</h2>
+      <h2 onClick={handleGenerate}>Jump to next lesson</h2>
     </div>
   )
 }
